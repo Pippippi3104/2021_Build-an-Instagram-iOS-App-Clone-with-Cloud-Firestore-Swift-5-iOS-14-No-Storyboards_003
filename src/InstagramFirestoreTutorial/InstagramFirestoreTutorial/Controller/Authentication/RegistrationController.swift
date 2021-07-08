92a0466e2665317539f0d6b2bc8db66c851f0af9
Satoshi Shimamura <>
2021-07-08 14:20:22 +0900

    note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by SatoshiShimamura on 2021/07/08.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
